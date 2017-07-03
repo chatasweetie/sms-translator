a2cdea3282b0989e04a901bd6d4acb7d76782ce1
Jessica <jessica.dene.earley@gmail.com>
2017-07-02 20:25:40 -0700

    added an example config file

---
+++ config_template.py
@@ -0,0 +1,22 @@
+PHONE_DIC = {
+        "Phone Number": {
+                    'language': 'key',
+                    'name':'name of person',
+                        },
+        "Phone Number": {
+                    'language': 'key',
+                    'name':'name of person',
+                        },
+}
+
+PARENTS = {
+        "mom": "Phone Number ",
+        "dad": "Phone Number",
+}
+
+FROM_PARENTS = {
+        "Phone Number" : "mom",
+        "Phone Number" : "dad",
+}
+
+MY_PHONE_NUMBER = "Phone Number"
